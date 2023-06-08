b7ef93381093e499dceae9ca2d6066eae1b40c1d
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2023-06-08 14:58:47 -0600

    Mejoramiento del aloritmo del 50% correcto sube al 83.33% correcto

--- es_primo.rb
+++ es_primo.rb
@@ -4,10 +4,20 @@
 n = gets.chomp.to_i
 
 unless n < 0
-  if (n != 1 and n % ((n+1)/2) != 0 and n % 9 != 0) or n == 2
+  if n != 1 or n == 2 and n < 3
     puts 1
   else
-    puts 0
+    i = 2
+    r = 1
+    while i < n
+      if n % i == 0
+        r = 0
+        break
+      else
+        (i % 2 == 0)? i += 1 : i += 2
+      end
+    end
+    puts (r == 1)? 1 : 0
   end
 else
   puts "el número debe ser positivo"
