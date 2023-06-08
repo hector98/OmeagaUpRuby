0992c91babb2405ae3886b8e92a06369d65176b3
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2023-06-07 20:41:57 -0600

    Comenzar reto es primo

---
+++ es_primo.rb
@@ -0,0 +1,14 @@
+# OmegaUp
+# Es primo
+
+n = gets.chomp.to_i
+
+unless n < 0
+  if (n != 1 and n % ((n+1)/2) != 0) or n == 2
+    puts 1
+  else
+    puts 0
+  end
+else
+  puts "el número debe ser positivo"
+end
